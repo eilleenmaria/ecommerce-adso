edf9d7dc221686ab98bd76061bee4dfc5b38471f
eilleen maria <egutierrezacosta85@gmail.com>
2023-10-10 15:39:10 -0500

    router lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI
+import { EMPTY_STRING, INTERNAL_PATHS } from '@data/constants/routes';
+import { SkeletonComponent } from '@layout/skeleton/skeleton.component';
+import { AuthModule } from '@modules/auth/auth.module';
+import { HomeModule } from '@modules/home/home.module';
 
+const routes: Routes = [
 
+  {
+		path: EMPTY_STRING,
+		component: SkeletonComponent,
+		children: [
+			{
+				path: INTERNAL_PATHS.CARIBBEAN_DEFAULT,
+				loadChildren: () => import('@modules/home/home.module').then((m): typeof HomeModule => m.HomeModule),
+			},
+      {
+				path: INTERNAL_PATHS.AUTH_DEFAULT,
+				loadChildren: () => import('@modules/auth/auth.module').then((m): typeof AuthModule => m.AuthModule),
+			},
+
+			{ path: '**', redirectTo: EMPTY_STRING, pathMatch: 'full' },
+		],
+	},
+	{ path: '**', redirectTo: EMPTY_STRING, pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
+  imports: [RouterModule.forRoot(routes, { useHash: true })
   ],
   exports: [RouterModule],
 })
